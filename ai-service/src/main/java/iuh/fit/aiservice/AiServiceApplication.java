8609d5bca2d9321be3501da516c8d2677572904f
Nguyen Quoc Huy <122158094+Nine99Huy8181@users.noreply.github.com>
2026-05-12 09:30:12 +0700

    feat: add config get properties from application

--- ai-service/src/main/java/iuh/fit/aiservice/AiServiceApplication.java
+++ ai-service/src/main/java/iuh/fit/aiservice/AiServiceApplication.java
@@ -1,9 +1,24 @@
 package iuh.fit.aiservice;
 
+import iuh.fit.aiservice.config.AiCacheProperties;
+import iuh.fit.aiservice.config.AiClientProperties;
+import iuh.fit.aiservice.config.AiGeminiProperties;
+import iuh.fit.aiservice.config.AiRagProperties;
+import iuh.fit.aiservice.config.AiRetryProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableConfigurationProperties({
+        AiGeminiProperties.class,
+        AiRagProperties.class,
+        AiRetryProperties.class,
+        AiCacheProperties.class,
+        AiClientProperties.class
+})
 public class AiServiceApplication {
 
     public static void main(String[] args) {
